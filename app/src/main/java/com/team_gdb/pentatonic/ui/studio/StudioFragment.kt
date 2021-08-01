72043ecca17c2e020ace4f4fef57ed7c50b18ad9
H43RO <h2is1234@naver.com>
2021-08-02 01:09:11 +0900

    [feat] 스튜디오 탭 추천 커버 리스트 레이아웃 추가

--- app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
@@ -1,9 +1,14 @@
 package com.team_gdb.pentatonic.ui.studio
 
 import android.content.Intent
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.adapter.CoverListAdapter
 import com.team_gdb.pentatonic.base.BaseFragment
 import com.team_gdb.pentatonic.databinding.FragmentStudioBinding
+import com.team_gdb.pentatonic.ui.lounge.LoungeFragmentDirections
+import com.team_gdb.pentatonic.ui.lounge.TestData
 import com.team_gdb.pentatonic.ui.record.RecordActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -11,6 +16,8 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
     override val layoutResourceId: Int
         get() = R.layout.fragment_studio
     override val viewModel: StudioViewModel by viewModel()
+
+    private lateinit var recommendCoverListAdapter: CoverListAdapter  // 추천 커버 리스트
 
     override fun initStartView() {
     }
@@ -22,5 +29,19 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
         binding.makeSoloCoverButton.setOnClickListener {
             startActivity(Intent(activity, RecordActivity::class.java))
         }
+
+        // 솔로 커버 리사이클러뷰 어댑터 생성
+        recommendCoverListAdapter = CoverListAdapter {
+            findNavController().navigate(StudioFragmentDirections.actionNavigationStudioToNavigationBandCover(it))
+        }
+
+        binding.recommendCoverList.apply {
+            this.adapter = recommendCoverListAdapter
+            this.layoutManager = LinearLayoutManager(context)
+            this.setHasFixedSize(true)
+        }
+
+        recommendCoverListAdapter.setItem(TestData.testSoloCoverList)
+
     }
 }
