1a12f7ea41cdcba400c63c755678e8f553851a90
haero_kim <mac-pro@kakao.com>
2021-08-05 11:31:20 +0900

    [feat] 위클리 챌린지 커버 리스트 구현

---
+++ app/src/main/java/com/team_gdb/pentatonic/adapter/CoverVerticalListAdapter.kt
@@ -0,0 +1,88 @@
+package com.team_gdb.pentatonic.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.data.model.CoverEntity
+import com.team_gdb.pentatonic.databinding.ItemHorizontalCoverListBinding
+import com.team_gdb.pentatonic.databinding.ItemVerticalCoverListBinding
+
+/**
+ * 커버 목록을 보여주기 위한 리사이클러뷰 어댑터
+ *
+ * @property itemClick  해당 커버 정보 페이지로 이동할 수 있도록 어댑터 생성 시 클릭리스너 동작 전달
+ */
+class CoverVerticalListAdapter(val itemClick: (CoverEntity) -> Unit) :
+    RecyclerView.Adapter<CoverVerticalListAdapter.ViewHolder>() {
+
+    private var coverEntityList: List<CoverEntity> = emptyList()  // Cover 아이템 리스트 정보
+
+    /**
+     * 레이아웃 바인딩 통한 ViewHolder 생성 후 반환
+     *
+     * @param parent    부모 ViewGroup
+     * @param viewType  리사이클러 뷰 뷰타입
+     * @return          생성된 ViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemVerticalCoverListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(coverEntityList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return coverEntityList.size
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemVerticalCoverListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(entity: CoverEntity) {
+                binding.coverNameTextView.text = entity.coverName
+                binding.coverOriginalSongTextView.text = entity.originalSong
+
+            if (entity.imageUrl.isNotBlank()) {
+                Glide.with(binding.root)
+                    .load(entity.imageUrl)
+                    .override(480, 272)
+                    .into(binding.coverImage)
+            } else {
+                Glide.with(binding.root)
+                    .load(R.drawable.placeholder_cover_bg)
+                    .override(480, 272)
+                    .into(binding.coverImage)
+            }
+
+//            val sessionSet = mutableSetOf<String>()
+//            var sessionListText = ""
+//            entity.sessionList.forEach {
+//                sessionSet.add(it.sessionName)
+//            }
+//            sessionSet.forEach {
+//                sessionListText += "$it "
+//            }
+
+
+            binding.coverSessionListTextView.text = "${entity.sessionList.size}명 참여중"
+
+            binding.coverLikeTextView.text = entity.like.toString()
+            binding.coverViewTextView.text = entity.view.toString()
+
+            binding.root.setOnClickListener {
+                itemClick(entity)
+            }
+        }
+    }
+
+    fun setItem(entities: List<CoverEntity>) {
+        this.coverEntityList = entities
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/team_gdb/pentatonic/ui/weekly_challenge/WeeklyChallengeFragment.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/weekly_challenge/WeeklyChallengeFragment.kt
@@ -1,24 +1,41 @@
 package com.team_gdb.pentatonic.ui.weekly_challenge
 
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.TestData
+import com.team_gdb.pentatonic.adapter.CoverHorizontalListAdapter
+import com.team_gdb.pentatonic.adapter.CoverVerticalListAdapter
 import com.team_gdb.pentatonic.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.team_gdb.pentatonic.databinding.FragmentWeeklyChallengeBinding
+import com.team_gdb.pentatonic.ui.lounge.LoungeFragmentDirections
 
-class WeeklyChallengeFragment : BaseFragment<FragmentWeeklyChallengeBinding, WeeklyChallengeViewModel>() {
+class WeeklyChallengeFragment :
+    BaseFragment<FragmentWeeklyChallengeBinding, WeeklyChallengeViewModel>() {
     override val viewModel: WeeklyChallengeViewModel by viewModel()
     override val layoutResourceId: Int
         get() = R.layout.fragment_weekly_challenge
 
+    private lateinit var weeklyChallengeCoverListAdapter: CoverVerticalListAdapter
+
     override fun initStartView() {
-//        TODO("Not yet implemented")
     }
 
     override fun initDataBinding() {
-//        TODO("Not yet implemented")
     }
 
     override fun initAfterBinding() {
-//        TODO("Not yet implemented")
+        weeklyChallengeCoverListAdapter = CoverVerticalListAdapter {
+            findNavController().navigate(
+                LoungeFragmentDirections.actionNavigationLoungeToNavigationBandCover(it)
+            )
+        }
+        binding.weeklyChallengeCoverList.apply {
+            this.layoutManager = LinearLayoutManager(context)
+            this.adapter = weeklyChallengeCoverListAdapter
+            this.setHasFixedSize(true)
+        }
+        weeklyChallengeCoverListAdapter.setItem(TestData.TEST_BAND_COVER_LIST)
     }
 }
