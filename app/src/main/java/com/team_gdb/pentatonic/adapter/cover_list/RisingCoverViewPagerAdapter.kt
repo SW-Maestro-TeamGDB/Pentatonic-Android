5cf32fa00e3aa8a0d0b16c526d5fc2fe93160fa3
H43RO <h2is1234@naver.com>
2021-08-12 23:41:56 +0900

    [feat] ViewPager 어댑터 구현 및 바인딩

---
+++ app/src/main/java/com/team_gdb/pentatonic/adapter/cover_list/RisingCoverViewPagerAdapter.kt
@@ -0,0 +1,72 @@
+package com.team_gdb.pentatonic.adapter.cover_list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.data.model.CoverEntity
+import com.team_gdb.pentatonic.databinding.ItemRisingCoverViewpagerBinding
+import com.team_gdb.pentatonic.databinding.ItemVerticalCoverListBinding
+
+/**
+ * 커버 목록을 보여주기 위한 리사이클러뷰 어댑터
+ *
+ * @property itemClick  해당 커버 정보 페이지로 이동할 수 있도록 어댑터 생성 시 클릭리스너 동작 전달
+ */
+class RisingCoverViewPagerAdapter(val itemClick: (CoverEntity) -> Unit) :
+    RecyclerView.Adapter<RisingCoverViewPagerAdapter.ViewHolder>() {
+
+    private var coverEntityList: List<CoverEntity> = emptyList()  // Cover 아이템 리스트 정보
+
+    /**
+     * 레이아웃 바인딩 통한 ViewHolder 생성 후 반환
+     *
+     * @param parent    부모 ViewGroup
+     * @param viewType  리사이클러 뷰 뷰타입
+     * @return          생성된 ViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemRisingCoverViewpagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(coverEntityList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return coverEntityList.size
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemRisingCoverViewpagerBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(entity: CoverEntity) {
+                binding.coverNameTextView.text = entity.coverName
+                binding.coverOriginalSongTextView.text = entity.originalSong
+
+            Glide.with(binding.root)
+                .load(entity.imageUrl)
+                .placeholder(R.drawable.placeholder_cover_bg)
+                .override(480, 272)
+                .into(binding.coverImage)
+
+            binding.coverSessionListTextView.text = "${entity.sessionDataList.size}"
+
+            binding.coverLikeTextView.text = entity.like.toString()
+            binding.coverViewTextView.text = entity.view.toString()
+
+            binding.root.setOnClickListener {
+                itemClick(entity)
+            }
+        }
+    }
+
+    fun setItem(entities: List<CoverEntity>) {
+        this.coverEntityList = entities
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
@@ -3,11 +3,13 @@ package com.team_gdb.pentatonic.ui.studio
 import android.content.Intent
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
 import com.team_gdb.pentatonic.R
 import com.team_gdb.pentatonic.adapter.cover_list.CoverHorizontalListAdapter
 import com.team_gdb.pentatonic.base.BaseFragment
 import com.team_gdb.pentatonic.databinding.FragmentStudioBinding
 import com.team_gdb.pentatonic.TestData
+import com.team_gdb.pentatonic.adapter.cover_list.RisingCoverViewPagerAdapter
 import com.team_gdb.pentatonic.adapter.song_list.SongHorizontalListAdapter
 import com.team_gdb.pentatonic.ui.create_cover.CreateCoverActivity
 import com.team_gdb.pentatonic.ui.record.RecordActivity
@@ -18,22 +20,23 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
         get() = R.layout.fragment_studio
     override val viewModel: StudioViewModel by viewModel()
 
-    private lateinit var recommendCoverListAdapter: CoverHorizontalListAdapter  // 추천 커버 리스트
+    private lateinit var recommendCoverViewPagerAdapter: RisingCoverViewPagerAdapter  // 추천 커버 리스트
     private lateinit var recommendSongListAdapter: SongHorizontalListAdapter  // 추천  리스트
 
 
     override fun initStartView() {
-        // 추천 커버 리사이클러뷰 어댑터 생성
-        recommendCoverListAdapter = CoverHorizontalListAdapter {
-            findNavController().navigate(StudioFragmentDirections.actionNavigationStudioToNavigationBandCover(it))
+        // 추천 커버 뷰 페이저 어댑터 생성
+        recommendCoverViewPagerAdapter = RisingCoverViewPagerAdapter {
+            findNavController().navigate(
+                StudioFragmentDirections.actionNavigationStudioToNavigationBandCover(
+                    it
+                )
+            )
         }
 
-        binding.recommendCoverList.apply {
-            this.layoutManager = LinearLayoutManager(context).apply {
-                this.orientation = LinearLayoutManager.HORIZONTAL
-            }
-            this.adapter = recommendCoverListAdapter
-            this.setHasFixedSize(true)
+        binding.recommendCoverViewPager.apply {
+            this.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            this.adapter = recommendCoverViewPagerAdapter
         }
 
         // 추천 곡 리사이클러뷰 어댑터 생성
@@ -70,15 +73,13 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
             startActivity(intent)
         }
 
-
-        recommendCoverListAdapter.setItem(TestData.TEST_SOLO_COVER_LIST)
-
+        recommendCoverViewPagerAdapter.setItem(TestData.TEST_BAND_COVER_LIST)
         recommendSongListAdapter.setItem(TestData.TEST_SONG_LIST)
     }
 
     companion object {
         const val COVER_MODE = "COVER_MODE"
         const val SOLO_COVER = "SOLO_COVER"
-        const val BAND_COVER ="BAND_COVER"
+        const val BAND_COVER = "BAND_COVER"
     }
 }
