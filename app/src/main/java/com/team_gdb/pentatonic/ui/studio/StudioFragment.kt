2994b48fd9ccccb0adcf635ec259d239e648aa97
H43RO <h2is1234@naver.com>
2021-08-13 00:09:52 +0900

    [feat] ViewPager 자동 스크롤 구현

--- app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioFragment.kt
@@ -1,6 +1,8 @@
 package com.team_gdb.pentatonic.ui.studio
 
 import android.content.Intent
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenResumed
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
@@ -13,6 +15,8 @@ import com.team_gdb.pentatonic.adapter.cover_list.RisingCoverViewPagerAdapter
 import com.team_gdb.pentatonic.adapter.song_list.SongHorizontalListAdapter
 import com.team_gdb.pentatonic.ui.create_cover.CreateCoverActivity
 import com.team_gdb.pentatonic.ui.record.RecordActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
@@ -25,6 +29,8 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
 
 
     override fun initStartView() {
+        binding.viewModel = this.viewModel
+
         // 추천 커버 뷰 페이저 어댑터 생성
         recommendCoverViewPagerAdapter = RisingCoverViewPagerAdapter {
             findNavController().navigate(
@@ -35,8 +41,16 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
         }
 
         binding.recommendCoverViewPager.apply {
-            this.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-            this.adapter = recommendCoverViewPagerAdapter
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            adapter = recommendCoverViewPagerAdapter
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                    //직접 유저가 스크롤했을 때 현재 위치 변경 처리
+                    viewModel.setCurrentPosition(position)
+                }
+            })
         }
 
         // 추천 곡 리사이클러뷰 어댑터 생성
@@ -45,15 +59,18 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
         }
 
         binding.recommendSongList.apply {
-            this.layoutManager = LinearLayoutManager(context).apply {
-                this.orientation = LinearLayoutManager.HORIZONTAL
+            layoutManager = LinearLayoutManager(context).apply {
+                orientation = LinearLayoutManager.HORIZONTAL
             }
-            this.adapter = recommendSongListAdapter
-            this.setHasFixedSize(true)
+            adapter = recommendSongListAdapter
+            setHasFixedSize(true)
         }
     }
 
     override fun initDataBinding() {
+        viewModel.currentPosition.observe(this) {
+            binding.recommendCoverViewPager.currentItem = it
+        }
     }
 
     override fun initAfterBinding() {
@@ -75,7 +92,23 @@ class StudioFragment : BaseFragment<FragmentStudioBinding, StudioViewModel>() {
 
         recommendCoverViewPagerAdapter.setItem(TestData.TEST_BAND_COVER_LIST)
         recommendSongListAdapter.setItem(TestData.TEST_SONG_LIST)
+
+        autoScrollViewPager()
     }
+
+    private fun autoScrollViewPager() {
+        lifecycleScope.launch {
+            while (true) {
+                whenResumed {
+                    delay(3000)
+                    viewModel.getCurrentPosition()?.let {
+                        viewModel.setCurrentPosition((it.plus(1)) % 3)
+                    }
+                }
+            }
+        }
+    }
+
 
     companion object {
         const val COVER_MODE = "COVER_MODE"
--- app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioViewModel.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/studio/StudioViewModel.kt
@@ -1,7 +1,19 @@
 package com.team_gdb.pentatonic.ui.studio
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.team_gdb.pentatonic.base.BaseViewModel
 
 class StudioViewModel : BaseViewModel() {
+    private val _currentPosition: MutableLiveData<Int> = MutableLiveData()
 
+    val currentPosition: LiveData<Int>
+        get() = _currentPosition
+
+
+    fun setCurrentPosition(position: Int){
+        _currentPosition.value = position
+    }
+
+    fun getCurrentPosition() = currentPosition.value
 }
