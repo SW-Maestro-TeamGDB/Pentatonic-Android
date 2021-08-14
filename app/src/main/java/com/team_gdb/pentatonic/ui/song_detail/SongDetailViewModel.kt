de1705121c6de8877e4df3e0fe53681e5573726c
haero_kim <mac-pro@kakao.com>
2021-08-14 12:18:45 +0900

    [feat] 곡 상세 정보 레이아웃 구현

--- app/src/main/java/com/team_gdb/pentatonic/ui/song_detail/SongDetailViewModel.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/song_detail/SongDetailViewModel.kt
@@ -6,13 +6,4 @@ import com.team_gdb.pentatonic.data.model.SongEntity
 
 class SongDetailViewModel : BaseViewModel() {
     val songEntity: MutableLiveData<SongEntity> = MutableLiveData()
-
-    val songTitle: MutableLiveData<String> = MutableLiveData()
-    val artistName: MutableLiveData<String> = MutableLiveData()
-    val albumTitle: MutableLiveData<String> = MutableLiveData()
-    val albumReleaseDate: MutableLiveData<String> = MutableLiveData()
-    val songGenre: MutableLiveData<String> = MutableLiveData()
-    val songWriter: MutableLiveData<String> = MutableLiveData()
-    val lyricist: MutableLiveData<String> = MutableLiveData()
-
 }
