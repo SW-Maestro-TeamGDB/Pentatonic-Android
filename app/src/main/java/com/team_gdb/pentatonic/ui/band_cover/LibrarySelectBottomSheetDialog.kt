b6056379759391fdd6ee77e3c21eba53fb97623b
haero_kim <mac-pro@kakao.com>
2021-08-16 17:15:36 +0900

    [feat] 라이브러리 선택 RecyclerView 어댑팅 구현

--- app/src/main/java/com/team_gdb/pentatonic/ui/band_cover/LibrarySelectBottomSheetDialog.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/band_cover/LibrarySelectBottomSheetDialog.kt
@@ -1,7 +1,11 @@
 package com.team_gdb.pentatonic.ui.band_cover
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.TestLibraryData
+import com.team_gdb.pentatonic.adapter.library.LibraryListAdapter
+import com.team_gdb.pentatonic.adapter.library.LibrarySelectListAdapter
 import com.team_gdb.pentatonic.base.BaseBottomSheetDialogFragment
 import com.team_gdb.pentatonic.databinding.DialogLibrarySelectBinding
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -12,8 +16,18 @@ class LibrarySelectBottomSheetDialog() :
         get() = R.layout.dialog_library_select
     override val viewModel: BandCoverViewModel by sharedViewModel()
 
+    private lateinit var librarySelectListAdapter: LibrarySelectListAdapter
+
     override fun initStartView() {
         binding.viewModel = this.viewModel
+        librarySelectListAdapter = LibrarySelectListAdapter {
+            // TODO : 라이브러리 내의 커버를 선택했을 때 동작 정의 (밴드 참여)
+        }
+        binding.libraryList.apply {
+            this.layoutManager = LinearLayoutManager(context)
+            this.adapter = librarySelectListAdapter
+            this.setHasFixedSize(true)
+        }
     }
 
     override fun initDataBinding() {
@@ -23,5 +37,6 @@ class LibrarySelectBottomSheetDialog() :
         binding.closeButton.setOnClickListener {
             dismiss()
         }
+        librarySelectListAdapter.setItem(TestLibraryData.TEST_LIBRARY_DATA)
     }
 }
