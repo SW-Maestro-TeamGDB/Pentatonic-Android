c81a2677d27000ce7e27b4ea0c1a08f9434b01bf
H43RO <h2is1234@naver.com>
2021-08-19 02:10:11 +0900

    [feat] 커버 파일 (녹음본) 업로드, 커버 정보 업로드 Repository 구현

--- app/src/main/java/com/team_gdb/pentatonic/repository/record_processing/RecordProcessingRepository.kt
+++ app/src/main/java/com/team_gdb/pentatonic/repository/record_processing/RecordProcessingRepository.kt
@@ -1,10 +1,11 @@
 package com.team_gdb.pentatonic.repository.record_processing
 
 import com.apollographql.apollo.api.Response
+import com.team_gdb.pentatonic.UploadCoverFileMutation
 import com.team_gdb.pentatonic.UploadCoverMutation
 import io.reactivex.rxjava3.core.Single
 
 interface RecordProcessingRepository {
-    fun uploadCoverFile(): Single<Response<UploadCoverMutation.Data>>
-    fun uploadCoverToLibrary(name: String, coverURI: String, songId: String, position: String)
+    fun uploadCoverFile(filePath: String): Single<Response<UploadCoverFileMutation.Data>>
+    fun uploadCoverToLibrary(name: String, coverURI: String, songId: String, position: String): Single<Response<UploadCoverMutation.Data>>
 }
--- app/src/main/java/com/team_gdb/pentatonic/repository/record_processing/RecordProcessingRepositoryImpl.kt
+++ app/src/main/java/com/team_gdb/pentatonic/repository/record_processing/RecordProcessingRepositoryImpl.kt
@@ -1,20 +1,37 @@
 package com.team_gdb.pentatonic.repository.record_processing
 
+import com.apollographql.apollo.api.FileUpload
 import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.rx3.rxMutate
+import com.team_gdb.pentatonic.UploadCoverFileMutation
 import com.team_gdb.pentatonic.UploadCoverMutation
+import com.team_gdb.pentatonic.network.NetworkHelper.apolloClient
+import com.team_gdb.pentatonic.type.CoverInput
+import com.team_gdb.pentatonic.type.SESSION_TYPE
+import com.team_gdb.pentatonic.type.UploadCoverFileInput
+import com.team_gdb.pentatonic.type.UploadCoverInput
 import io.reactivex.rxjava3.core.Single
 
-class RecordProcessingRepositoryImpl: RecordProcessingRepository {
-    override fun uploadCoverFile() {
-//        TODO("Not yet implemented")
-    }
+class RecordProcessingRepositoryImpl : RecordProcessingRepository {
+    override fun uploadCoverFile(filePath: String): Single<Response<UploadCoverFileMutation.Data>> =
+        apolloClient.rxMutate(
+            UploadCoverFileMutation(
+                UploadCoverFileInput(file = FileUpload("audio/mpeg3", filePath))
+            )
+        )
 
     override fun uploadCoverToLibrary(
         name: String,
         coverURI: String,
         songId: String,
         position: String
-    ) {
-//        TODO("Not yet implemented")
-    }
+    ): Single<Response<UploadCoverMutation.Data>> = apolloClient.rxMutate(
+        UploadCoverMutation(
+            UploadCoverInput(
+                CoverInput(
+                    name, coverURI, songId, position = SESSION_TYPE.valueOf(position)
+                )
+            )
+        )
+    )
 }
