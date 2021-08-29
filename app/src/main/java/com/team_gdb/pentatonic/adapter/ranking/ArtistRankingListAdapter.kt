d48689d724126e1805b52ee9496fb23129f16fd2
H43RO <h2is1234@naver.com>
2021-08-29 22:19:48 +0900

    [feat] 밴드 & 아티스트 랭킹 리사이클러뷰 어댑터 구현

---
+++ app/src/main/java/com/team_gdb/pentatonic/adapter/ranking/ArtistRankingListAdapter.kt
@@ -0,0 +1,71 @@
+package com.team_gdb.pentatonic.adapter.ranking
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.data.model.CoverEntity
+import com.team_gdb.pentatonic.data.model.UserEntity
+import com.team_gdb.pentatonic.databinding.ItemArtistRankingListBinding
+import com.team_gdb.pentatonic.databinding.ItemBandRankingListBinding
+import com.team_gdb.pentatonic.databinding.ItemVerticalCoverListBinding
+
+/**
+ * 아티스트 랭킹을 보여주기 위한 리사이클러뷰 어댑터
+ *
+ * @property itemClick  해당 커버 정보 페이지로 이동하는 동작
+ */
+class ArtistRankingListAdapter(val itemClick: (UserEntity) -> Unit) :
+    RecyclerView.Adapter<ArtistRankingListAdapter.ViewHolder>() {
+
+    private var coverEntityList: List<UserEntity> = emptyList()  // Cover 아이템 리스트 정보
+
+    /**
+     * 레이아웃 바인딩 통한 ViewHolder 생성 후 반환
+     *
+     * @param parent    부모 ViewGroup
+     * @param viewType  리사이클러 뷰 뷰타입
+     * @return          생성된 ViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemArtistRankingListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(coverEntityList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return if (coverEntityList.size > 3) 3 else coverEntityList.size
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemArtistRankingListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(entity: UserEntity) {
+            // 커버 대표 이미지
+            Glide.with(binding.root)
+                .load(entity.profileImage)
+                .placeholder(R.drawable.placeholder_cover_bg)
+                .override(480, 272)
+                .into(binding.artistProfileImage)
+
+            // 아티스트명
+            binding.artistNameTextView.text = entity.username
+
+            // 해당 커버를 클릭하면, 커버 페이지로 이동
+            binding.root.setOnClickListener {
+                itemClick(entity)
+            }
+        }
+    }
+
+    fun setItem(entities: List<UserEntity>) {
+        this.coverEntityList = entities
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/team_gdb/pentatonic/adapter/ranking/BandRankingListAdapter.kt
@@ -0,0 +1,73 @@
+package com.team_gdb.pentatonic.adapter.ranking
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.team_gdb.pentatonic.R
+import com.team_gdb.pentatonic.data.model.CoverEntity
+import com.team_gdb.pentatonic.databinding.ItemBandRankingListBinding
+import com.team_gdb.pentatonic.databinding.ItemVerticalCoverListBinding
+
+/**
+ * 커버 랭킹을 보여주기 위한 리사이클러뷰 어댑터
+ *
+ * @property itemClick  해당 커버 정보 페이지로 이동하는 동작
+ */
+class BandRankingListAdapter(val itemClick: (CoverEntity) -> Unit) :
+    RecyclerView.Adapter<BandRankingListAdapter.ViewHolder>() {
+
+    private var coverEntityList: List<CoverEntity> = emptyList()  // Cover 아이템 리스트 정보
+
+    /**
+     * 레이아웃 바인딩 통한 ViewHolder 생성 후 반환
+     *
+     * @param parent    부모 ViewGroup
+     * @param viewType  리사이클러 뷰 뷰타입
+     * @return          생성된 ViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemBandRankingListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(coverEntityList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return if (coverEntityList.size > 3) 3 else coverEntityList.size
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemBandRankingListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(entity: CoverEntity) {
+            // 커버 대표 이미지
+            Glide.with(binding.root)
+                .load(entity.imageUrl)
+                .placeholder(R.drawable.placeholder_cover_bg)
+                .override(480, 272)
+                .into(binding.coverImage)
+
+            // 커버명과 원곡명
+            binding.coverNameTextView.text = entity.coverName
+            binding.coverOriginalSongTextView.text = entity.originalSong
+
+            // 좋아요수
+            binding.coverLikeTextView.text = entity.like.toString()
+
+            // 해당 커버를 클릭하면, 커버 페이지로 이동
+            binding.root.setOnClickListener {
+                itemClick(entity)
+            }
+        }
+    }
+
+    fun setItem(entities: List<CoverEntity>) {
+        this.coverEntityList = entities
+        notifyDataSetChanged()
+    }
+}
