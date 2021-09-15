9b4c17e52a6c4f4639e99536d62923813790f0c1
H43RO <h2is1234@naver.com>
2021-09-15 21:35:38 +0900

    [feat] 밴드 커버 감상 전 세션 선택 페이지 구현

--- app/src/main/java/com/team_gdb/pentatonic/ui/session_select/SessionSelectActivity.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/session_select/SessionSelectActivity.kt
@@ -1,21 +1,27 @@
-package com.team_gdb.pentatonic.ui.band_cover
+package com.team_gdb.pentatonic.ui.session_select
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
 import com.team_gdb.pentatonic.R
 import com.team_gdb.pentatonic.adapter.cover_view.SessionConfigListAdapter
 import com.team_gdb.pentatonic.base.BaseActivity
 import com.team_gdb.pentatonic.data.model.CoverEntity
-import com.team_gdb.pentatonic.databinding.ActivityBandCoverBinding
+import com.team_gdb.pentatonic.databinding.ActivitySessionSelectBinding
+import com.team_gdb.pentatonic.ui.band_cover.BandCoverActivity
+import com.team_gdb.pentatonic.ui.band_cover.BandCoverViewModel
+import com.team_gdb.pentatonic.ui.band_cover.LibrarySelectBottomSheetDialog
+import com.team_gdb.pentatonic.ui.lounge.LoungeFragment
 import com.team_gdb.pentatonic.ui.lounge.LoungeFragment.Companion.COVER_ENTITY
 import com.team_gdb.pentatonic.ui.profile.ProfileActivity
-import com.team_gdb.pentatonic.ui.session_select.SessionSelectActivity
+import org.koin.android.ext.android.bind
+
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class BandCoverActivity : BaseActivity<ActivityBandCoverBinding, BandCoverViewModel>() {
+class SessionSelectActivity : BaseActivity<ActivitySessionSelectBinding, BandCoverViewModel>() {
     override val layoutResourceId: Int
-        get() = R.layout.activity_band_cover
+        get() = R.layout.activity_session_select
     override val viewModel: BandCoverViewModel by viewModel()
 
     private val coverEntity: CoverEntity by lazy {
@@ -23,13 +29,14 @@ class BandCoverActivity : BaseActivity<ActivityBandCoverBinding, BandCoverViewMo
     }
     private lateinit var sessionListAdapter: SessionConfigListAdapter
 
+
     override fun initStartView() {
         binding.viewModel = this.viewModel
-        binding.coverNameTextView.text = coverEntity.coverName
-        binding.coverIntroductionTextView.text = coverEntity.introduction
+        binding.lifecycleOwner = this
+
         sessionListAdapter = SessionConfigListAdapter({
             val intent = Intent(this, ProfileActivity::class.java)
-            intent.putExtra(USER_ENTITY, it)
+            intent.putExtra(BandCoverActivity.USER_ENTITY, it)
             startActivity(intent)
         }, {
             val bottomSheetDialog = LibrarySelectBottomSheetDialog()
@@ -40,36 +47,15 @@ class BandCoverActivity : BaseActivity<ActivityBandCoverBinding, BandCoverViewMo
             this.adapter = sessionListAdapter
             this.setHasFixedSize(true)
         }
-
-        Glide.with(this)
-            .load(coverEntity.imageUrl)
-            .placeholder(R.drawable.placeholder_cover_bg)
-            .override(480, 272)
-            .into(binding.coverImage)
-
-        binding.coverLikeTextView.text = coverEntity.like.toString()
-        binding.coverViewTextView.text = coverEntity.view.toString()
-
     }
 
     override fun initDataBinding() {
     }
 
     override fun initAfterBinding() {
-        sessionListAdapter.setItem(coverEntity.sessionDataList)
-
         binding.backButton.setOnClickListener {
             finish()
         }
-
-        binding.bandPlayButton.setOnClickListener {
-            val intent = Intent(this, SessionSelectActivity::class.java)
-            intent.putExtra(COVER_ENTITY, coverEntity)
-            startActivity(intent)
-        }
-    }
-
-    companion object {
-        const val USER_ENTITY = "USER_ENTITY"
+        sessionListAdapter.setItem(coverEntity.sessionDataList)
     }
 }
