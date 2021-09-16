da7f3470fbb36c93e3c8d52fe175212e28d094a7
H43RO <h2is1234@naver.com>
2021-09-16 18:19:52 +0900

    [fix] 참여 세션 HashMap 로직 일부 수정
    - 같은 세션의 같은 사람에 대한 요청이 들어온 경우, 선택 해제 동작을 하기 위해 관련 로직을 추가하였음.

--- app/src/main/java/com/team_gdb/pentatonic/ui/band_cover/BandCoverViewModel.kt
+++ app/src/main/java/com/team_gdb/pentatonic/ui/band_cover/BandCoverViewModel.kt
@@ -4,20 +4,22 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import com.team_gdb.pentatonic.base.BaseViewModel
-import com.team_gdb.pentatonic.data.model.UserEntity
-import com.team_gdb.pentatonic.data.session.SessionSetting
 
 
 class BandCoverViewModel : BaseViewModel() {
-    val selectedSession: HashMap<String, String> = hashMapOf()
+    private val selectedSession: HashMap<String, String> = hashMapOf()
     val selectedSessionLiveData: MutableLiveData<HashMap<String, String>> = MutableLiveData()
 
     @RequiresApi(Build.VERSION_CODES.N)
-    fun addSession(sessionName: String, userName: String){
-        if (selectedSession.containsKey(sessionName)){  // 이미 있는 세션인 경우 Replace
-            selectedSession.replace(sessionName, userName)
-        } else{  // 새로운 세션인 경우 put
-            selectedSession.put(sessionName, userName)
+    fun addSession(sessionName: String, userName: String) {
+        if (selectedSession.containsKey(sessionName)) {  // 이미 있는 세션인 경우
+            if (selectedSession[sessionName] == userName) {  // 같은 사람에 대한 요청인 경우 세션 정보 자체를 제거
+                selectedSession.remove(sessionName)
+            } else {
+                selectedSession.replace(sessionName, userName)  // 일반적인 경우 유저 정보를 replace
+            }
+        } else {  // 새로운 세션인 경우 put
+            selectedSession[sessionName] = userName
         }
         selectedSessionLiveData.postValue(selectedSession)
     }
