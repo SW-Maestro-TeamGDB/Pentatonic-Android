9ef1089881f41da7e6b46bac3b09e84bbec2fc4e
haero_kim <mac-pro@kakao.com>
2021-09-21 13:20:39 +0900

    [feat] 스키마 업데이트 및 떠오르는 밴드 API 연결

--- app/src/main/java/com/team_gdb/pentatonic/repository/lounge/LoungeRepository.kt
+++ app/src/main/java/com/team_gdb/pentatonic/repository/lounge/LoungeRepository.kt
@@ -1,8 +1,13 @@
 package com.team_gdb.pentatonic.repository.lounge
 
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.rx3.rxQuery
+import com.team_gdb.pentatonic.GetTrendBandsQuery
 import com.team_gdb.pentatonic.data.model.SongEntity
+import com.team_gdb.pentatonic.network.NetworkHelper.apolloClient
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
 interface LoungeRepository {
-
+    fun getTrendBands(): Observable<Response<GetTrendBandsQuery.Data>>
 }
