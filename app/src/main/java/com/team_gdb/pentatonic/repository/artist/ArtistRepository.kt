41523911a86fd09c56358d0811e7a9888dc7e51f
H43RO <h2is1234@naver.com>
2021-10-11 23:01:12 +0900

    [feat] 아티스트 랭킹, 커버 랭킹 쿼리 구현

--- app/src/main/java/com/team_gdb/pentatonic/repository/artist/ArtistRepository.kt
+++ app/src/main/java/com/team_gdb/pentatonic/repository/artist/ArtistRepository.kt
@@ -1,4 +1,11 @@
 package com.team_gdb.pentatonic.repository.artist
 
+import com.apollographql.apollo.api.Response
+import com.team_gdb.pentatonic.GetRankedBandListQuery
+import com.team_gdb.pentatonic.GetRankedUserListQuery
+import io.reactivex.rxjava3.core.Observable
+
 interface ArtistRepository {
+    fun getRankedUserList(): Observable<Response<GetRankedUserListQuery.Data>>
+    fun getRankedBandList(): Observable<Response<GetRankedBandListQuery.Data>>
 }
--- app/src/main/java/com/team_gdb/pentatonic/repository/artist/ArtistRepositoryImpl.kt
+++ app/src/main/java/com/team_gdb/pentatonic/repository/artist/ArtistRepositoryImpl.kt
@@ -1,5 +1,16 @@
 package com.team_gdb.pentatonic.repository.artist
 
-class ArtistRepositoryImpl : ArtistRepository {
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.rx3.rxQuery
+import com.team_gdb.pentatonic.GetRankedBandListQuery
+import com.team_gdb.pentatonic.GetRankedUserListQuery
+import com.team_gdb.pentatonic.network.NetworkHelper.apolloClient
+import io.reactivex.rxjava3.core.Observable
 
+class ArtistRepositoryImpl : ArtistRepository {
+    override fun getRankedUserList(): Observable<Response<GetRankedUserListQuery.Data>> =
+        apolloClient.rxQuery(GetRankedUserListQuery())
+
+    override fun getRankedBandList(): Observable<Response<GetRankedBandListQuery.Data>> =
+        apolloClient.rxQuery(GetRankedBandListQuery())
 }
